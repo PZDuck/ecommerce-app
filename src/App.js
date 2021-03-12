0c95b8818ea13f9bb9e405dab3f33b0798c40651
PZDuck <agp2281337@gmail.com>
2021-03-12 12:50:22 -0500

    initial header, correct commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
+import Header from "./components/Nav/Header";
 
 const App = () => {
   return (
     <Switch>
+      <Header />
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />

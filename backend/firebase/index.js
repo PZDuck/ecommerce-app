8d7dd5dd8ca5bbe75b0c03371cb492c0ed0d71f1
PZDuck <agp2281337@gmail.com>
2021-03-18 23:23:18 -0400

    initial backend setup; firebase setup; base auth and user routes

---
+++ backend/firebase/index.js
@@ -0,0 +1,8 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ecommerce-app-65b03.firebaseio.com",
+});
